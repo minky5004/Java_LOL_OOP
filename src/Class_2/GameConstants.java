9d644cbed0851fad88931093a52db3b906f9e18e
minky5004 <minky5004@gmail.com>
2025-12-15 14:15:56 +0900

    Static Final로 Ashe Garen 추가

---
+++ src/Class_2/GameConstants.java
@@ -0,0 +1,15 @@
+package Class_2;
+
+public class GameConstants {
+
+    public static final int MAX_LEVEL = 18;
+
+    public static final int GAREN_HP = 690;
+    public static final int GAREN_AD = 69;
+    public static final int GAREN_DF = 38;
+
+    public static final int ASHED_HP = 610;
+    public static final int ASHED_AD = 59;
+    public static final int ASHED_DF = 26;
+
+}
